a447bc1226784ca1eec33f1c411629bf98f25fad
BundW32 <barth@bundwimmobilien.de>
2025-12-31 16:59:27 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // base: './' sorgt dafür, dass die App auch unter www.beispiel.de/unterordner/ funktioniert
-  base: './', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
