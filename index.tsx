2eb1a224e01f6f970a69106e62e1c85e7d50cab0
BundW32 <barth@bundwimmobilien.de>
2025-12-31 17:53:25 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// FIX: Curly braces imports the exact "App" function
+// FIX: Curly braces { } import the Named Export
 import { App } from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error("Failed to find the root element");
-}
+if (!rootElement) throw new Error("Root element not found");
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
